393454b8dea878fae6a017de1618df5801f9e996
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-10-09 23:38:27 +0800

    Update Name.java
    Minimal message update.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-        "No contacts found for Names should only contain alphanumeric characters and spaces, and it should not be blank";
+        "No contacts found for given Name. Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
