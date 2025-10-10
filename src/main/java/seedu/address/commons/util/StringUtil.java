707be84247efc482e9be6f45a94ca4790adc4ee0
zengjing <zjgoalsforlife312@gmail.com>
2025-10-10 23:38:34 +0800

    Correct files with check fails

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -29,13 +29,14 @@ public class StringUtil {
 
         String preppedWord = word.trim().toLowerCase();
         checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
+        checkArgument(preppedWord.split("\\s+").length == 1,
+                "Word parameter should be a single word");
 
-        String preppedSentence = sentence;
+        String preppedSentence = sentence.toLowerCase().trim();
         String[] wordsInPreppedSentence = preppedSentence.split("\\s+");
 
         return Arrays.stream(wordsInPreppedSentence)
-                .anyMatch(wordInSentence -> wordInSentence.toLowerCase().contains(preppedWord) );
+                .anyMatch(wordInSentence -> wordInSentence.toLowerCase().contains(preppedWord));
     }
 
     /**
