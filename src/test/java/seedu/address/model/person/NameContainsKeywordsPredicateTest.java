fdf8bebeb5a947ea57d725bc9b66aa272784d4e5
Hypovolemic <weifengwong1018@gmail.com>
2025-10-14 04:31:00 +0800

    Update to pass checkstyle

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
