22fce3cec01d2931a5d2e723fa62c42e5917f91f
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-10-14 21:39:45 +0800

    Fix checkstle errors

--- src/main/java/seedu/address/ui/DeletePopup.java
+++ src/main/java/seedu/address/ui/DeletePopup.java
@@ -5,8 +5,8 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import seedu.address.model.person.Person;
 
